022c2bbd29ddaaab0361e0f8d5426f792beff016
Greg Gardini <ggard214@gmail.com>
2020-10-20 10:10:22 -0700

    second set of sorts

--- problems/02-selection-sort.js
+++ problems/02-selection-sort.js
@@ -2,11 +2,23 @@
 
 // Implement swap without looking at bubble sort
 function swap(arr, index1, index2) {
-  // your code here
+  let newArr = arr[index1];
+  arr[index1] = arr[index2];
+  arr[index2] = newArr;
 }
+let arr1 = [1,2,3,4,5,8,9];
+swap(arr1, 0, 1);
+console.log(arr1);
 
-function selectionSort(list) {
-  // your code here
+function selectionSort(list) { // [3,2,1]
+  for (let i = 0; i < list.length; i++) { // i = 3
+    for (let j = i + 1; j < list.length; j++) { // j = 2
+      let min = list[i]; // min = 3
+      if (list[j] < min) { // If 2 is less than 3 
+        swap(list, i, j); // [2, 3, 1]
+      }
+    }
+  }
 }
 
 module.exports = {
--- problems/03-insertion-sort.js
+++ problems/03-insertion-sort.js
@@ -1,7 +1,16 @@
 // Implement Insertion Sort
 
 function insertionSort(list) {
-  // your code here
+  // let firstEl = list[0];
+  for (let i = 1; i < list.length; i++) {
+    for (let j = i + 1; j >= 0; j--) {
+
+    }
+    if (list[i] < list[i - 1]) {
+      list[i - 1] = list[i];
+    }
+  }
+  return list;
 }
 
 module.exports = {
