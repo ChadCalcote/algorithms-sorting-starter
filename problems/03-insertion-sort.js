87dac01813cb4dbc8b20199517b16ebcc9cfa367
Greg Gardini <ggard214@gmail.com>
2020-10-20 13:59:03 -0700

    merge sort

--- problems/03-insertion-sort.js
+++ problems/03-insertion-sort.js
@@ -2,10 +2,10 @@
 
 function insertionSort(list) { // [5, 2, 4, 6, 1, 3]
     // let firstEl = list[0];
-  for (i = 1; i < list.length; i++) { // 
-    let currentNum = list[i]; //
-    for (j = i - 1; j >= 0 && currentNum < list[j]; j--) {
-      list[j + 1] = list[j];
+  for (i = 1; i < list.length; i++) { // i = 2
+    let currentNum = list[i]; // currentNum = i = 2
+    for (j = i - 1; j >= 0 && currentNum < list[j]; j--) { // j = 0, 0 = 5, iterate backwards
+      list[j + 1] = list[j]; // j(5) moves to the 2nd spot in the array [2,4,6.1,3] [5, 6]
     }
     list[j + 1] = currentNum;
   }
--- problems/04-merge-sort.js
+++ problems/04-merge-sort.js
@@ -1,14 +1,41 @@
 // Implement Merge Sort
 
 function merge(array1, array2) {
-  // your code here
+  let mergedArray = [];
+
+  while (array1.length && array2.length) {
+    if (array1[0] > array2[0]) {
+      mergedArray.push(array2.shift());
+    } else {
+      mergedArray.push(array1.shift());
+    }
+  }
+  while (array1.length) {
+    mergedArray.push(array1.shift());
+  }
+  while (array2.length) {
+    mergedArray.push(array2.shift());
+  }
+  return mergedArray;
 }
 
 function mergeSort(array) {
-  // your code here
+  if (array.length <= 1) {
+    return array;
+  }
+
+  let midIdx = Math.floor(array.length / 2);
+  let leftHalf = array.slice(0, midIdx);
+  let rightHalf = array.slice(midIdx);
+
+  let sortedLeft = mergeSort(leftHalf);
+  let sortedRight = mergeSort(rightHalf);
+
+  return merge(sortedLeft, sortedRight);
 }
+
 
 module.exports = {
   merge,
-  mergeSort
+  mergeSort,
 };
