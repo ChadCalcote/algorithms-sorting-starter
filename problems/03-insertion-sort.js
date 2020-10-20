97f0c77d08ee31ffe9caed0b95bea06c73b6c682
Chad Calcote <calcote@att.net>
2020-10-20 13:33:59 -0600

    work on problem 03

--- problems/03-insertion-sort.js
+++ problems/03-insertion-sort.js
@@ -1,17 +1,18 @@
 // Implement Insertion Sort
 
-function insertionSort(list) {
-  // let firstEl = list[0];
-  for (let i = 1; i < list.length; i++) {
-    for (let j = i + 1; j >= 0; j--) {
-
+function insertionSort(list) { // [5, 2, 4, 6, 1, 3]
+    // let firstEl = list[0];
+  for (i = 1; i < list.length; i++) { // 
+    let currentNum = list[i]; //
+    for (j = i - 1; j >= 0 && currentNum < list[j]; j--) {
+      list[j + 1] = list[j];
     }
-    if (list[i] < list[i - 1]) {
-      list[i - 1] = list[i];
-    }
+    list[j + 1] = currentNum;
   }
   return list;
 }
+
+// [1, 2, 3, 4, 5, 6]
 
 module.exports = {
   insertionSort
